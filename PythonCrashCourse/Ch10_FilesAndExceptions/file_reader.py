ac969b41cdb479dcd59e6e89e56d2f4260b1830b
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-08-27 13:08:31 -0700

    reorganized directories

---
+++ PythonCrashCourse/Ch10_FilesAndExceptions/file_reader.py
@@ -0,0 +1,6 @@
+''' open() takes one argument: the name of the file you want to open
+    Python looks for the file in the directory where the program currently being executed is stored
+    open() returns an object representing the file
+    keyword 'with' closes the file once access to it is no longer needed
+        files can explicitly be closed with close()
+'''
